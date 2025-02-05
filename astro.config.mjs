2cba46abb3eb7ccc3586efedc931f178bbb109dd
Imrane QERCHI <imrane.qerchi@edu.univ-fcomte.fr>
2025-02-05 10:42:47 +0100

    Ajout de l'URL & mise en forme générale

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   devToolbar : {enabled:false},
-  site: "https://example.com",
+  site: "https://zingy-boba-8391b3.netlify.app/",
   integrations: [preact()]
 });
--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,9 +1,9 @@
-import { h } from 'preact';
-import { useState } from 'preact/hooks';
+import { h } from "preact";
+import { useState } from "preact/hooks";
 
-export default function Greeting({messages}) {
-
-  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+export default function Greeting({ messages }) {
+  const randomMessage = () =>
+    messages[Math.floor(Math.random() * messages.length)];
 
   const [greeting, setGreeting] = useState(messages[0]);
 
