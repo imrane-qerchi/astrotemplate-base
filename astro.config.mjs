388f1802912e0b12d5ae645baee7c65a61d802c7
Imrane QERCHI <imrane.qerchi@edu.univ-fcomte.fr>
2025-02-05 10:07:18 +0100

    Ajout de RSS et Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    devToolbar : {enabled:false},
+  devToolbar : {enabled:false},
+  site: "https://example.com",
+  integrations: [preact()]
 });
